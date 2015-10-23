e40e9ca7cd393b53ac556575b79e10e6f8685d19
Álvaro Saco <alvarosaco@gmail.com>
2015-10-22 22:42:42 -0500

    Actualizaciones varias.

---
+++ assets/js/owl-carousels.js
@@ -0,0 +1,136 @@
+        $(document).ready(function() {            
+            $("#speakers-1").owlCarousel({
+                items : 5,
+             
+                //Autoplay
+                autoPlay : true,
+                stopOnHover : false,
+                slideSpeed: 100,
+                paginationSpeed : 1000,
+                rewindSpeed : 1000
+            });
+
+            $("#speakers-2").owlCarousel({
+                items : 5,
+             
+                //Autoplay
+                autoPlay : true,
+                stopOnHover : false,
+                slideSpeed: 100,
+                paginationSpeed : 4000,
+                rewindSpeed : 4000
+
+            });
+
+            $("#speakers-3").owlCarousel({
+                items : 5,
+             
+                //Autoplay
+                autoPlay : true,
+                stopOnHover : false
+            });
+
+            $("#speakers-4").owlCarousel({
+                items : 5,
+             
+                //Autoplay
+                autoPlay : true,
+                stopOnHover : false
+            });
+
+            $("#speakers-5").owlCarousel({
+                items : 5,
+             
+                //Autoplay
+                autoPlay : true,
+                stopOnHover : false
+            });
+
+            $("#speakers-6").owlCarousel({
+                items : 5,
+             
+                //Autoplay
+                autoPlay : true,
+                stopOnHover : false
+            });
+
+            $("#speakers-7").owlCarousel({
+                items : 5,
+             
+                //Autoplay
+                autoPlay : true,
+                stopOnHover : false
+            });
+
+            $("#speakers-8").owlCarousel({
+                items : 5,
+             
+                //Autoplay
+                autoPlay : true,
+                stopOnHover : false
+            });
+
+            $("#speakers-9").owlCarousel({
+                items : 5,
+             
+                //Autoplay
+                autoPlay : true,
+                stopOnHover : false
+            });
+
+
+
+            $("#team-members-1").owlCarousel({
+                items : 5,
+             
+                //Autoplay
+                autoPlay : true,
+                stopOnHover : false
+            });
+
+            $("#team-members-2").owlCarousel({
+                items : 5,
+             
+                //Autoplay
+                autoPlay : true,
+                stopOnHover : false
+            });            
+
+            $("#team-members-3").owlCarousel({
+                          
+                items : 5,
+                autoPlay : true,
+                scrollPerPage : false,
+                // Responsive 
+                responsive: true,                
+            });            
+
+            $("#partners-xl").owlCarousel({
+                items : 1,
+             
+                //Autoplay
+                autoPlay : true,
+                stopOnHover : false
+            });
+
+            $("#partners-l").owlCarousel({
+                items : 2,
+             
+                //Autoplay
+                autoPlay : true,
+                stopOnHover : false
+            });
+
+            $("#partners-m").owlCarousel({
+                items : 3,
+             
+                //Autoplay
+                autoPlay : true,
+                stopOnHover : false
+            });
+
+            $("#media-partners").owlCarousel({
+                items : 1,
+                             
+            });
+        });
